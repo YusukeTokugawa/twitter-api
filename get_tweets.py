52af810afd40c81e71218963538d484e0be2ae47
tokugawa_y@worksap.co.jp <tokugawa_y@worksap.co.jp>
2018-11-15 18:44:23 +0800

    Modified to be able to scrape twitter with query (hastag)

--- get_tweets.py
+++ get_tweets.py
@@ -7,6 +7,7 @@ Created on Nov 13, 2018
 from tweet_fields import TweetFields
 from data_formatter import DataFormatter
 from bs4 import BeautifulSoup
+import random
 import sys
 import os
 import json
@@ -24,6 +25,16 @@ class GetTweets:
     def __init__(self):
         pass
 
+    # Without the header, can not get tweet data with 'URL_FOR_USER_NAME' URL
+    HEADERS_LIST = [
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13',
+    'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
+    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+    'Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre'
+    ]
+    HEADER = {'User-Agent': random.choice(HEADERS_LIST)}
+
     def get_dict_tweets_list(self, limit,query, url):
         data_formatter = DataFormatter()
         dict_tweets_list=[]
@@ -33,8 +44,8 @@ class GetTweets:
                 base_url = url.format(q=query,pos="")
             else:
                 base_url = url.format(q=query,pos=next_position)
-            url_data = urllib.request.urlopen(base_url)
-            content = json.loads(url_data.read().decode('utf8'))
+            url_data = requests.get(base_url, headers=self.HEADER)
+            content = json.loads(url_data.text)
             next_position = content['min_position']
             html_data = content['items_html']
             soup = BeautifulSoup(html_data)
@@ -62,6 +73,7 @@ class GetTweets:
         return dict_tweets_list
 
     def get_tweets_by_hashtags(self, hashtag, limit):
+         hashtag = '%23'+hashtag #Add hash mark
          dict_tweets_list = self.get_dict_tweets_list(limit, hashtag, self.URL_FOR_HASHTAG)
          return dict_tweets_list
 
